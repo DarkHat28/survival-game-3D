38808539c6a4b6c4662affd24908bbb6c2dc37ac
Mohd DSaif Khan <140172956+DarkHat28@users.noreply.github.com>
2025-08-31 13:42:28 +0530

    Simple Reticle

--- Scripts/Characters/Player/UI/reticle.gd
+++ Scripts/Characters/Player/UI/reticle.gd
@@ -29,13 +29,13 @@ func adjust_reticle_lines():
 	var vel: Vector3 = player.velocity
 	var origin: Vector3 = Vector3.ZERO
 	var pos: Vector2 = Vector2.ZERO
-	var speed: float = origin.distance_to(vel)
+	var speed: float = origin.distance_to(vel) 
 	
 	# Adjust Reticle line positions
-	reticle_lines[0].position = lerp(reticle_lines[0].position, pos + Vector2(-speed * reticle_distance, 0.0), reticle_distance) #Left
-	reticle_lines[1].position = lerp(reticle_lines[1].position, pos + Vector2(speed * reticle_distance, 0.0), reticle_distance) #Right
-	reticle_lines[2].position = lerp(reticle_lines[2].position, pos + Vector2(0.0, -speed * reticle_distance), reticle_distance) #Top
-	reticle_lines[3].position = lerp(reticle_lines[3].position, Vector2(0.0, speed * reticle_distance), reticle_distance) #Bottom
+	reticle_lines[0].position = lerp(reticle_lines[0].position, pos + Vector2(-speed * reticle_distance, 0.0), 1) #Left
+	reticle_lines[1].position = lerp(reticle_lines[1].position, pos + Vector2(speed * reticle_distance, 0.0), 1.0) #Right
+	reticle_lines[2].position = lerp(reticle_lines[2].position, pos + Vector2(0.0, -speed * reticle_distance), 1.0) #Top
+	reticle_lines[3].position = lerp(reticle_lines[3].position, Vector2(0.0, speed * reticle_distance), 1.0) #Bottom
 
 func apply_line_colors():
 	for line in reticle_lines:
