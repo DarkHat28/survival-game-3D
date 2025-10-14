5e4dafc3edc4f1488a3471b44920ed32d0f37036
Mohd DSaif Khan <140172956+DarkHat28@users.noreply.github.com>
2025-10-14 21:34:43 +0530

    0.4.5.1

--- Scripts/UI/InGame/throwable_ui.gd
+++ Scripts/UI/InGame/throwable_ui.gd
@@ -1,45 +1,45 @@
 class_name GrenadeUI
 extends Control
 
-@onready var grenade_icon: TextureButton = %GrenadeIcon
-@onready var grenade_list: VBoxContainer = %GrenadeList
-@onready var frag_grenade: TextureButton = %FragGrenade
-@onready var motolov_cocktail: TextureButton = %MotolovCocktail
+@onready var frag_grenade: Button = %FragGrenade
+@onready var motolov_cocktail: Button = %MotolovCocktail
+@onready var grenade_icon: Button = %GrenadeIcon
+@onready var container: PanelContainer = %Container
 
 enum GrenadeType { FRAG, MOLOTOV }
 @export var current_grenade: GrenadeType = GrenadeType.FRAG
 
 
 func _ready():
+	container.visible = false
 	update_texture()
-	# All connections will be made in the editor
-	grenade_list.visible = false
-	#grenade_icon.texture_normal = current_grenade.texture_normal
 
 func toggle_list():
-	grenade_list.visible = !grenade_list.visible
+	container.visible = !container.visible
+
 
 func _on_grenade_icon_pressed():
 	toggle_list()
+
+func _on_motolov_cocktail_pressed() -> void:
+	current_grenade = GrenadeType.MOLOTOV
 
 
 func _on_frag_grenade_pressed():
 	current_grenade = GrenadeType.FRAG
 
-func _on_motolov_pressed() -> void:
-	current_grenade = GrenadeType.MOLOTOV
+
 
 func update_texture() -> void:
 	match current_grenade:
 		GrenadeType.FRAG:
 			print("Frag Texture")
-			grenade_icon.texture_normal = frag_grenade.texture_normal
+			grenade_icon.icon = frag_grenade.icon
 			frag_grenade.disabled = false
 			motolov_cocktail.disabled = true
 		
 		GrenadeType.MOLOTOV:
 			print("Motolov Texture")
-			grenade_icon.texture_normal = motolov_cocktail.texture_normal
+			grenade_icon.icon = motolov_cocktail.icon
 			frag_grenade.disabled = true
 			motolov_cocktail.disabled = false
-	
