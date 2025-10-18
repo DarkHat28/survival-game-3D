8855a175a1e1ca0386f0de75dd897cdae295b7eb
DrajSkr <146635680+DrajSkr@users.noreply.github.com>
2025-10-18 14:41:19 +0530

    0.12.1

--- Scripts/Characters/Player/enemy_2.gd
+++ Scripts/Characters/Player/enemy_2.gd
@@ -1,20 +1,24 @@
 extends CharacterBody3D
 
 @export var movement_speed: float = 100.0
-@export var door_open_time: float = 1
 
+
+var chasing:bool = false
 var prev_pos:Vector3 = Vector3.ZERO
-var range_dist :float = 30
+var range_dist :float = 10
 
 @onready var navigation_agent: NavigationAgent3D = $NavigationAgent3D
 
 var target_position: Vector3
+var gravity :float = 12
 
 func _ready():
 	await get_tree().process_frame
 	set_random_target()
 
 func _physics_process(delta):
+	var dist = global_position.distance_to(Global.player_pos)
+	chasing=true if dist<range_dist else false
 	rotation_degrees.x=0
 	rotation_degrees.z=0
 
@@ -26,12 +30,16 @@ func _physics_process(delta):
 	velocity = direction * movement_speed * delta
 	if velocity.length() > 0.01:
 		look_at(global_position - velocity.normalized(), Vector3.UP)
+	if not is_on_floor():
+		velocity.y -= gravity
 	move_and_slide()
+	anim()
 
 func set_random_target():
 	var nav_map = navigation_agent.get_navigation_map()
 	if NavigationServer3D.map_get_iteration_id(nav_map) <= 0:
 		return
+	
 	
 	var random_point = NavigationServer3D.map_get_random_point(nav_map,1,1)
 	var closest_point = NavigationServer3D.map_get_closest_point(nav_map, random_point)
@@ -50,3 +58,10 @@ func _on_timer_timeout():
 	if(global_position.distance_to(prev_pos)<=0.001):
 		set_random_target()
 	prev_pos=global_position
+
+func anim():
+	pass
+	if(chasing):
+		#run anim
+		pass
+	
